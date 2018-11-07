1d0d853fd4c3ba1bdca90c153eca70b2d5dc4adf
LittleTry <ht@yuancore.com>
2018-11-07 11:22:04 +0800

    @author ht

--- src/main/java/top/huangt/springboot/SpringbootApplication.java
+++ src/main/java/top/huangt/springboot/SpringbootApplication.java
@@ -3,6 +3,9 @@ package top.huangt.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ht
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
